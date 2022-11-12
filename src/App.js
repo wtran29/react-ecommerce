cde2fdc1aea061e52fbb32252230ea132f08cdc9
Bi11y G04t <wtran292gmail.com>
2022-11-11 17:05:31 -0800

    included react-router, using routes, route, outlet to structure nav bar and shop

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Home from "./routes/home/home";
 
-import './components/directory/category-list.scss';
-import CategoryList from './components/directory/category-list';
+const Nav = () => {
+  return (
+    <div>
+      <div>
+        <h1>Nav bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
+
+const Shop = () => {
+  return <h1>Shop Items</h1>
+}
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/YN8tWR5/categories-hats.jpg"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/CB1L00V/categories-jackets.jpg"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/YWMvkL7/categories-sneakers.jpg"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/Gcy4MjS/categories-womens.jpg"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/wW4Rjrt/categories-mens.jpg"
-    }
-  ];
-
-return (
-    <div className="App">
-      <CategoryList categories={categories} />
-    </div>
-  );
+  return (
+  <Routes>
+    <Route path='/' element={<Nav />}>
+      <Route index element={<Home />} />
+      <Route path='shop' element={<Shop />} />
+    </Route>
+  </Routes>
+  )
 };
 
 export default App;
---
+++ src/routes/home/home.jsx
@@ -0,0 +1,45 @@
+import { Outlet } from 'react-router';
+
+import '../../components/directory/category-list.scss';
+import CategoryList from '../../components/directory/category-list';
+
+const Home = () => {
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/YN8tWR5/categories-hats.jpg"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/CB1L00V/categories-jackets.jpg"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/YWMvkL7/categories-sneakers.jpg"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/Gcy4MjS/categories-womens.jpg"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/wW4Rjrt/categories-mens.jpg"
+    }
+  ];
+
+return (
+    <div className="App">
+        
+      <CategoryList categories={categories} />
+      <Outlet />
+    </div>
+  );
+};
+
+
+export default Home;
